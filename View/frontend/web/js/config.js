bb97207819f3a2286da2dc23b6b528bfc7dc1699
Desmond <dinhtp@mageplaza.com>
2018-12-28 10:09:57 +0700

    Complete Common JS Config File

---
+++ View/frontend/web/js/config.js
@@ -0,0 +1,28 @@
+define([
+    'jquery',
+    'uiComponent'
+], function ($, component) {
+    'use strict';
+    return component.extend({
+        initialize: function (config) {
+            require(['https://static.addtoany.com/menu/page.js']);
+
+            var buttonColor = config.buttonColor,
+                iconColor = config.iconColor,
+                onClick = config.click,
+                popUp = config.popUp,
+                noService = config.service;
+
+            var a2a = $('.a2a_kit');
+
+            var a2aJS = '<script type="text/javascript">';
+            a2aJS += 'var a2a_config = a2a_config || {};';
+            a2aJS += 'a2a_config.icon_color ="'+ buttonColor + ',' + iconColor+'";';
+            a2aJS += 'a2a_config.num_services = '+ noService +';';
+            a2aJS += 'a2a_config.onclick = '+ onClick +';';
+            a2aJS += 'a2a_config.thanks = {postShare: '+ popUp +',};';
+            a2aJS += '</script>';
+            a2a.append(a2aJS);
+        }
+    });
+});
