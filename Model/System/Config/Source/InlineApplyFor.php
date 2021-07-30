6769b9c8df679b2030f6d88043f36926c34a4bc4
Lê Thế Vinh <vinhlt@mageplaza.com>
2021-07-30 15:14:10 +0700

    --compatible Daily Deals

--- Model/System/Config/Source/InlineApplyFor.php
+++ Model/System/Config/Source/InlineApplyFor.php
@@ -30,6 +30,10 @@ class InlineApplyFor extends OptionArray
     const HOME_PAGE     = 'home_page';
     const CATEGORY_PAGE = 'category_page';
     const PRODUCT_PAGE  = 'product_page';
+    const DAILYDEAL_ALLDEALS  = 'dailydeal_alldeals';
+    const DAILYDEAL_BESTSELLERDEALS  = 'dailydeal_bestsellerdeals';
+    const DAILYDEAL_FEATUREDDEALS  = 'dailydeal_featureddeals';
+    const DAILYDEAL_NEWDEALS  = 'dailydeal_newdeals';
 
     /**
      * @return array
@@ -40,6 +44,10 @@ class InlineApplyFor extends OptionArray
             self::HOME_PAGE     => __('Home Page'),
             self::CATEGORY_PAGE => __('Categories Page'),
             self::PRODUCT_PAGE  => __('Products Page'),
+            self::DAILYDEAL_ALLDEALS  => __('Daily Deal All Deals Page'),
+            self::DAILYDEAL_BESTSELLERDEALS  => __('Daily Deal Bestseller Deals Page'),
+            self::DAILYDEAL_FEATUREDDEALS  => __('Daily Deal Featured Deals Page'),
+            self::DAILYDEAL_NEWDEALS  => __('Daily Deal New Deals Page'),
         ];
     }
 }
